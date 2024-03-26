973ee6a5e44fb940ef1226b1c43cb70ee0934f2f
conor shields <cshields024@gmail.com>
2024-03-26 19:17:05 -0400

    ex for study hall

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,27 +1,45 @@
-import { Router } from 'express'
-import Todo from '../models/todo.model.js'
-import validateSession from '../middleware/validate-session.js'
+import { Router } from "express";
+import Todo from "../models/todo.model.js";
+import validateSession from "../middleware/validate-session.js";
 
-const router = Router()
+const router = Router();
 
 // Added validate Session which will allow us to connect the new todo to the user who created it.
-router.post('/', validateSession, async(req, res) => {
+router.post("/", validateSession, async (req, res) => {
   try {
-    const { todo } = req.body
+    const { todo } = req.body;
     const myTodo = new Todo({
       todo,
-      owner_id: req.user._id
-    })
-    const newTodo = await myTodo.save()
+      owner_id: req.user._id,
+    });
+    const newTodo = await myTodo.save();
     res.status(200).json({
       message: `${newTodo.todo} added to list`,
-      newTodo
-    })
+      newTodo,
+    });
   } catch (error) {
     res.status(500).json({
-      message: error.message
-    })
+      message: error.message,
+    });
   }
-})
+});
 
-export default router
+router.get("/", async (req, res) => {
+  try {
+    const allTodos = await Todo.find();
+    allTodos
+      ? res.status(200).json({
+          message: "success",
+          allTodos,
+        })
+      : res.status(404).json({
+          message: "no todos",
+        });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
+export default router;
